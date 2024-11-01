62749f8c67bf4b862447f1c5946df42decdd4223
Franziska Menti <fmenti@phys.ethz.ch>
2024-11-01 17:45:58 +0100

    added docstring to all sdb.py functions and prepared file for integration test

--- data_generation/life_td_data_generation/provider/sdb.py
+++ data_generation/life_td_data_generation/provider/sdb.py
@@ -1,10 +1,9 @@
 """ 
-Generates the data for the database for each of the data providers separately. 
+Acquires and processes the data from the provider sdb. 
 Requires server connection to SIMBAD.
 """
 
 import numpy as np #arrays
-from astropy import io
 from astropy.table import Table
 from datetime import datetime
 
@@ -13,12 +12,23 @@ from utils.io import stringtoobject, save, Path
 from sdata import empty_cat
 from provider.utils import fetch_main_id, IdentifierCreator, fill_sources_table, create_sources_table, create_provider_table
 
-def create_sdb_helpertable(distance_cut_in_pc,sdb_ref):
+
+
+def create_sdb_helpertable(distance_cut_in_pc,sdb_ref,data):
+    """
+    Creates helper table.
+    
+    :param float distance_cut_in_pc: Distance up to which stars are included.
+    :param str sdb_ref: Reference information for the sdb data.
+    :param data: Table containing sdb data.
+    :type data: astropy.table.table.Table
+    :returns: Helper table.
+    :rtype: astropy.table.table.Table
+    """
     plx_in_mas_cut=1000./distance_cut_in_pc
 
     #loading table obtained via direct communication from Grant Kennedy
-    sdb_helptab=io.votable.parse_single_table(
-        Path().additional_data+"sdb_30pc_09_02_2024.xml").to_table()
+    sdb_helptab=data
     #transforming from string type into object to have variable length
     sdb_helptab=stringtoobject(sdb_helptab,212)
     #removing objects with plx_value=='None' or masked entries
@@ -47,18 +57,42 @@ def create_sdb_helpertable(distance_cut_in_pc,sdb_ref):
     return sdb_helptab
 
 def create_h_link_table(sdb_helptab):
+    """
+    Creates hierarchical link table.
+    
+    :param sdb_helptab: Sdb helper table.
+    :type sdb_helptab: astropy.table.table.Table
+    :returns: Hierarchical link table.
+    :rtype: astropy.table.table.Table
+    """
     h_link=sdb_helptab['id','main_id','disks_ref']
     h_link.rename_columns(['main_id','disks_ref'],
                              ['parent_main_id','h_link_ref'])
     h_link.rename_column('id','main_id')
     return h_link
 
-def create_objects_table(sdb_helptab):        
+def create_objects_table(sdb_helptab): 
+    """
+    Creates objects table.
+    
+    :param sdb_helptab: Sdb helper table.
+    :type sdb_helptab: astropy.table.table.Table
+    :returns: Objects table.
+    :rtype: astropy.table.table.Table
+    """
     objects_table=sdb_helptab['id','ids','type']
     objects_table.rename_column('id','main_id')
     return objects_table
 
 def create_ident_table(sdb_helptab):
+    """
+    Creates identifier table.
+    
+    :param sdb_helptab: Sdb helper table.
+    :type sdb_helptab: astropy.table.table.Table
+    :returns: Identifier table.
+    :rtype: astropy.table.table.Table
+    """
     ident_table=sdb_helptab['ids','id','disks_ref']
     # would prefer to use id instad of ids paremeter but this raises an error
     # so I use ids which has the same content as id
@@ -66,6 +100,16 @@ def create_ident_table(sdb_helptab):
     return ident_table
 
 def create_sdb_sources_table(sdb_helptab,sdb):
+    """
+    Creates sources table.
+    
+    :param sdb_helptab: Sdb helper table.
+    :type sdb_helptab: astropy.table.table.Table
+    :param sdb: Dictionary of database table names and tables.
+    :type sdb: dict(str,astropy.table.table.Table)
+    :returns: Sources table.
+    :rtype: astropy.table.table.Table
+    """
     tables=[sdb['provider'],sdb_helptab]
     ref_columns=[['provider_bibcode'],['disks_ref']]
     sources_table=create_sources_table(tables,ref_columns,
@@ -73,6 +117,14 @@ def create_sdb_sources_table(sdb_helptab,sdb):
     return sources_table
 
 def create_disk_basic_table(sdb_helptab):
+    """
+    Creates basic disk data table.
+    
+    :param sdb_helptab: Sdb helper table.
+    :type sdb_helptab: astropy.table.table.Table
+    :returns: Basic disk data table.
+    :rtype: astropy.table.table.Table
+    """
     disk_basic=sdb_helptab['id','rdisk_bb','e_rdisk_bb','disks_ref']    
     for column in ['rdisk_bb','e_rdisk_bb']:
         if len(sdb_helptab[column].mask.nonzero()[0])>0:
@@ -82,11 +134,13 @@ def create_disk_basic_table(sdb_helptab):
     disk_basic=disk_basic[np.where(np.isfinite(disk_basic['rad_value']))]
     return disk_basic
 
-def provider_sdb(distance_cut_in_pc):
+def provider_sdb(distance_cut_in_pc,data):
     """
     Optains and arranges disk data.
 
-    :returnss: Dictionary with names and astropy tables containing
+    :param float distance_cut_in_pc: Distance up to which stars are included.
+    :param data: Table containing sdb data.
+    :returns: Dictionary with names and astropy tables containing
         reference data, provider data, object data, identifier data, object to 
         object relation data and basic disk data.
     :rtype: dict(str,astropy.table.table.Table)
@@ -95,10 +149,9 @@ def provider_sdb(distance_cut_in_pc):
     sdb = empty_cat.copy()
     sdb['provider'] = create_provider_table('Grant Kennedy Disks',
                                         'http://drgmk.com/sdb/',
-                                        'priv. comm.','2024-02-09')
-    
+                                        'priv. comm.','2024-02-09')    
     sdb_helptab = create_sdb_helpertable(distance_cut_in_pc,
-                          sdb['provider']['provider_bibcode'][0])
+                          sdb['provider']['provider_bibcode'][0],data)
 
     sdb['h_link']=create_h_link_table(sdb_helptab)
     sdb_helptab['ids']=sdb_helptab['id']#because only single id per source given
