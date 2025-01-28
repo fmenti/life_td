51f05be9a1417e05a081902549efa2e72ab77746
Franziska Menti <fmenti@phys.ethz.ch>
2025-01-28 17:58:36 +0100

    fixed integration test building stuff working now for different distance cuts. not all functions updated to new standard though yet

--- data_generation/life_td_data_generation/integration_tests/integration_test_building.py
+++ data_generation/life_td_data_generation/integration_tests/integration_test_building.py
@@ -6,6 +6,20 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 from astropy.table import Table
 
+#not showing plots
+show_kw=True #<--- added
+#show_kw=False
+    
+if show_kw:   #<--- added
+    plt.ion() #<--- added
+
+def approxvalue(dist, val,x):
+    poly = np.polyfit(dist,val,1)
+    return poly[0]*x+poly[1]
+
+with open(Path().data+'distance_cut.txt', 'r') as h:  
+    distance_cut=float(h.readlines()[0])
+
 def test_data_makes_sense_main_id():
     # experimental numbers
     dist_dist = Table(data=[ [    5,    10,    30],
@@ -17,8 +31,6 @@ def test_data_makes_sense_main_id():
                  names=['dist','total','st','sy','pl','di'], 
                  dtype=[float, float,float,float,float,float])
     
-    with open(Path().data+'distance_cut.txt', 'r') as h:  
-        distance_cut=float(h.readlines()[0])
     [table]=load(['objects'],location=Path().data)
     data=table['main_id','type']
 
@@ -42,13 +54,6 @@ def test_data_makes_sense_mass_st():
     data=different_data(arr_with_potential_fill_values)
     bins=10
 
-    #not showing plots
-    show_kw=True #<--- added
-    #show_kw=False
-    
-    if show_kw:   #<--- added
-        plt.ion() #<--- added
-
     fig=plt.figure()
     y, bins2,patches=plt.hist(data, bins, density=True)
     #careful density=True means probability density (area under histogram integrates to 1
@@ -68,12 +73,17 @@ def test_data_makes_sense_mass_st():
     plt.title('mes_mass_st')
     plt.scatter(x,y)
     plt.plot(x_model, y_model, color='r')
-    plt.show()    
+    plt.show()
+
+    # values for dist were tested before
+    a = approxvalue([30,5],[0.5,3.0],distance_cut)
+    b = approxvalue([30,5],[1,18],distance_cut)
+    c = approxvalue([30,5],[-0.1,0.2],distance_cut)
 
     #assert
-    assert a_opt <3 and a_opt >0.5
-    assert b_opt <8 and b_opt >1
-    assert c_opt <0.2 and a_opt >-0.1
+    assert a_opt <a+a/10. and a_opt >a-a/10.
+    assert b_opt <b+b/10. and b_opt >b-b/10.
+    assert c_opt <c+c/10. and a_opt >c-c/10.
 
 def test_data_makes_sense_mass_pl():
     #data
@@ -141,7 +151,7 @@ def test_data_makes_sense_mag_i():
     
     arr=different_data(data)
 
-    bins=100
+    bins=10
     y, bins2,patches=plt.hist(arr, bins, density=True, alpha=0.6, color='g')
 
     xmin, xmax = plt.xlim()
@@ -152,8 +162,12 @@ def test_data_makes_sense_mag_i():
 
     plt.plot(x, y_fit, color='r')
     plt.show()
-    assert mu <10 and mu >9
-    assert std <4 and std >3
+
+    m = approxvalue([30,5],[10,8],distance_cut)
+    s = approxvalue([30,5],[4,3.8],distance_cut)
+    
+    assert mu <m+m/10. and mu > m-m/10.
+    assert std <s+s/10. and std >s-s/10.
 
 
     
