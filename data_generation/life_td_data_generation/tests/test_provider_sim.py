d39a609d713f1c38b0db5241925d80b6795bafd2
Franziska Menti <fmenti@phys.ethz.ch>
2025-08-14 14:09:14 +0200

    included test for create_h_link_table

--- data_generation/life_td_data_generation/tests/test_provider_sim.py
+++ data_generation/life_td_data_generation/tests/test_provider_sim.py
@@ -1,8 +1,9 @@
 from provider.simbad import *
-from astropy.table import Table, setdiff
+from astropy.table import Table, setdiff, MaskedColumn
 import provider.simbad as simbad_module
 from sdata import empty_dict
 import pytest
+from utils.io import stringtoobject
 
 @pytest.fixture
 def query_returns(monkeypatch):
@@ -10,6 +11,7 @@ def query_returns(monkeypatch):
     distance_cut_in_pc = 5
     test_objects = []
 
+    membership = MaskedColumn([None,None,100], name='membership', mask=[True, True,False])
     t0 = Table({
         'main_id': ['star1','star2','star3'],
         'coo_ra': [160.,165.,100.],
@@ -27,7 +29,7 @@ def query_returns(monkeypatch):
         'plx_value': [190.,230.,375.],
         'plx_ref': ['ref_plx','ref_plx','ref_plx2'],
         'plx_qual': ['A','A','C'],
-        'membership': [None,None,100.],
+        'membership': membership,
         'parent_oid': [4,None,4],
         'h_link_ref': ['ref_hlink','','ref_hlink'],
         'otypes': ['*|**','BD?','*|**'],
@@ -38,11 +40,11 @@ def query_returns(monkeypatch):
     })
     parents_without_plx = t0[:0].copy()
     parents_without_plx.add_row(['system1',None,None,None,None,None,4,
-                                 '','','','','',None,None,'','',None,None,
+                                 '','','','','',None,None,'','',np.ma.masked,None,
                                  '','**','system1',None,None,None])
     children_without_plx = t0[:0].copy()
     children_without_plx.add_row(['planet1',None,None,None,None,None,5,
-                                 '','','','','',None,None,'','',None,3,
+                                 '','','','','',None,None,'','',np.ma.masked,3,
                                  'ref_hlink2','Pl','planet1',None,None,None])
 
     ident = Table({
@@ -100,12 +102,43 @@ def test_create_ident_table(query_returns):
 
     assert len(setdiff(return_ident, expected_ident)) == 0
 
+def test_create_h_link_table(query_returns,monkeypatch):
+    # Data
+    result_sim_helptab, result_sim, t0, return_ident = query_returns
+    result_stars = creating_helpertable_stars(result_sim_helptab, result_sim)
+    main_id_table = Table({
+        'main_id': ['star1','star3', 'planet1'],
+        'parent_main_id': ['system1','system1', 'star3'],
+        'h_link_ref': ['ref_hlink','ref_hlink','ref_hlink2'],
+        'membership': [np.ma.masked,100,np.ma.masked],
+        'parent_oid': [1,4,3]
+    })
+
+    def fake_fetch_main_id(sim_h_link, id_creator):
+        return main_id_table
+
+
+    monkeypatch.setattr(simbad_module, 'fetch_main_id', fake_fetch_main_id)
+
+    # Execute
+    result_h_link = create_h_link_table(result_sim_helptab, result_sim, result_stars)
+    #issue: wrong null value in membership column. find out what simbad returns, str, need to have them as object.
+    # sim returns int and masked entries
+
+    expected_h_link = Table({
+        'main_id': ['star1','star3', 'planet1'],
+        'parent_main_id': ['system1','system1', 'star3'],
+        'h_link_ref': ['ref_hlink','ref_hlink','ref_hlink2'],
+        'membership': [999999, 100,999999]
+    })
+
+    # Verify
+    assert len(setdiff(result_h_link, expected_h_link)) == 0
+
     # this stuff below here is for further testing functions
 
     minimal_simbad = empty_dict.copy()
-    minimal_simbad['provider'] = create_provider_table('SIMBAD',
-                                                       "http://simbad.u-strasbg.fr:80/simbad/sim-tap",
-                                                       '2000A&AS..143....9W')
+
     minimal_simbad['objects'] = Table({
         'type': ['st'],
         'ids': ['id1|star1'],
