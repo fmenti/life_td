5d853a1e62e115b22a5c880d2243bd0e48921040
Franziska Menti <fmenti@phys.ethz.ch>
2025-08-26 13:48:11 +0200

    cosmetic changes by tooling ruff to sdb and test files

--- data_generation/life_td_data_generation/provider/sdb.py
+++ data_generation/life_td_data_generation/provider/sdb.py
@@ -1,20 +1,23 @@
-""" 
-Acquires and processes the data from the provider sdb. 
+"""
+Acquires and processes the data from the provider sdb.
 Requires server connection to SIMBAD.
 """
 
-import numpy as np  #arrays
-
-#self created modules
-from utils.io import stringtoobject, save
+import numpy as np  # arrays
+from provider.utils import (
+    IdentifierCreator,
+    create_provider_table,
+    create_sources_table,
+    fetch_main_id
+)
 from sdata import empty_dict
-from provider.utils import fetch_main_id, IdentifierCreator, create_sources_table, create_provider_table
+from utils.io import save, stringtoobject
 
 
 def create_sdb_helpertable(distance_cut_in_pc, sdb_ref, data):
     """
     Creates helper table.
-    
+
     :param float distance_cut_in_pc: Distance up to which stars are included.
     :param str sdb_ref: Reference information for the sdb data.
     :param data: Table containing sdb data.
@@ -22,88 +25,92 @@ def create_sdb_helpertable(distance_cut_in_pc, sdb_ref, data):
     :returns: Helper table.
     :rtype: astropy.table.table.Table
     """
-    plx_in_mas_cut = 1000. / distance_cut_in_pc
+    plx_in_mas_cut = 1000.0 / distance_cut_in_pc
 
-    #loading table obtained via direct communication from Grant Kennedy
+    # loading table obtained via direct communication from Grant Kennedy
     sdb_helptab = data
-    #transforming from string type into object to have variable length
+    # transforming from string type into object to have variable length
     sdb_helptab = stringtoobject(sdb_helptab, 212)
-    #removing objects with plx_value=='None' or masked entries
-    if len(sdb_helptab['plx_value'].mask.nonzero()[0]) > 0:
-        print('careful, masked entries in plx_value')
+    # removing objects with plx_value=='None' or masked entries
+    if len(sdb_helptab["plx_value"].mask.nonzero()[0]) > 0:
+        print("careful, masked entries in plx_value")
 
-    #sorting out everything with plx_value too big
-    sdb_helptab = sdb_helptab[np.where(sdb_helptab['plx_value'] > plx_in_mas_cut)]
-    #adds the column for object type
-    sdb_helptab['type'] = ['di' for j in range(len(sdb_helptab))]
-    sdb_helptab['disks_ref'] = [sdb_ref
-                                for j in range(len(sdb_helptab))]
-    #making sure identifiers are unique
-    ind = sdb_helptab.group_by('id').groups.indices
+    # sorting out everything with plx_value too big
+    sdb_helptab = sdb_helptab[
+        np.where(sdb_helptab["plx_value"] > plx_in_mas_cut)
+    ]
+    # adds the column for object type
+    sdb_helptab["type"] = ["di" for j in range(len(sdb_helptab))]
+    sdb_helptab["disks_ref"] = [sdb_ref for j in range(len(sdb_helptab))]
+    # making sure identifiers are unique
+    ind = sdb_helptab.group_by("id").groups.indices
     for i in range(len(ind) - 1):
         l = ind[i + 1] - ind[i]
         if l == 2:
-            sdb_helptab['id'][ind[i]] = sdb_helptab['id'][ind[i]] + 'a'
-            sdb_helptab['id'][ind[i] + 1] = sdb_helptab['id'][ind[i] + 1] + 'b'
+            sdb_helptab["id"][ind[i]] = sdb_helptab["id"][ind[i]] + "a"
+            sdb_helptab["id"][ind[i] + 1] = sdb_helptab["id"][ind[i] + 1] + "b"
         if l > 2:
-            print('more than two disks with same name')
-    #fetching updated main identifier of host star from simbad
-    sdb_helptab.rename_column('main_id', 'sdb_host_main_id')
-    sdb_helptab = fetch_main_id(sdb_helptab,
-                                IdentifierCreator(name='main_id', colname='sdb_host_main_id'))
+            print("more than two disks with same name")
+    # fetching updated main identifier of host star from simbad
+    sdb_helptab.rename_column("main_id", "sdb_host_main_id")
+    sdb_helptab = fetch_main_id(
+        sdb_helptab,
+        IdentifierCreator(name="main_id", colname="sdb_host_main_id"),
+    )
     return sdb_helptab
 
 
 def create_h_link_table(sdb_helptab):
     """
     Creates hierarchical link table.
-    
+
     :param sdb_helptab: Sdb helper table.
     :type sdb_helptab: astropy.table.table.Table
     :returns: Hierarchical link table.
     :rtype: astropy.table.table.Table
     """
-    h_link = sdb_helptab['id', 'main_id', 'disks_ref']
-    h_link.rename_columns(['main_id', 'disks_ref'],
-                          ['parent_main_id', 'h_link_ref'])
-    h_link.rename_column('id', 'main_id')
+    h_link = sdb_helptab["id", "main_id", "disks_ref"]
+    h_link.rename_columns(
+        ["main_id", "disks_ref"], ["parent_main_id", "h_link_ref"]
+    )
+    h_link.rename_column("id", "main_id")
     return h_link
 
 
 def create_objects_table(sdb_helptab):
     """
     Creates objects table.
-    
+
     :param sdb_helptab: Sdb helper table.
     :type sdb_helptab: astropy.table.table.Table
     :returns: Objects table.
     :rtype: astropy.table.table.Table
     """
-    objects_table = sdb_helptab['id', 'ids', 'type']
-    objects_table.rename_column('id', 'main_id')
+    objects_table = sdb_helptab["id", "ids", "type"]
+    objects_table.rename_column("id", "main_id")
     return objects_table
 
 
 def create_ident_table(sdb_helptab):
     """
     Creates identifier table.
-    
+
     :param sdb_helptab: Sdb helper table.
     :type sdb_helptab: astropy.table.table.Table
     :returns: Identifier table.
     :rtype: astropy.table.table.Table
     """
-    ident_table = sdb_helptab['ids', 'id', 'disks_ref']
+    ident_table = sdb_helptab["ids", "id", "disks_ref"]
     # would prefer to use id instad of ids paremeter but this raises an error
     # so I use ids which has the same content as id
-    ident_table.rename_columns(['ids', 'disks_ref'], ['main_id', 'id_ref'])
+    ident_table.rename_columns(["ids", "disks_ref"], ["main_id", "id_ref"])
     return ident_table
 
 
 def create_sdb_sources_table(sdb_helptab, sdb):
     """
     Creates sources table.
-    
+
     :param sdb_helptab: Sdb helper table.
     :type sdb_helptab: astropy.table.table.Table
     :param sdb: Dictionary of database table names and tables.
@@ -111,29 +118,32 @@ def create_sdb_sources_table(sdb_helptab, sdb):
     :returns: Sources table.
     :rtype: astropy.table.table.Table
     """
-    tables = [sdb['provider'], sdb_helptab]
-    ref_columns = [['provider_bibcode'], ['disks_ref']]
-    sources_table = create_sources_table(tables, ref_columns,
-                                         sdb['provider']['provider_name'][0])
+    tables = [sdb["provider"], sdb_helptab]
+    ref_columns = [["provider_bibcode"], ["disks_ref"]]
+    sources_table = create_sources_table(
+        tables, ref_columns, sdb["provider"]["provider_name"][0]
+    )
     return sources_table
 
 
 def create_disk_basic_table(sdb_helptab):
     """
     Creates basic disk data table.
-    
+
     :param sdb_helptab: Sdb helper table.
     :type sdb_helptab: astropy.table.table.Table
     :returns: Basic disk data table.
     :rtype: astropy.table.table.Table
     """
-    disk_basic = sdb_helptab['id', 'rdisk_bb', 'e_rdisk_bb', 'disks_ref']
-    for column in ['rdisk_bb', 'e_rdisk_bb']:
+    disk_basic = sdb_helptab["id", "rdisk_bb", "e_rdisk_bb", "disks_ref"]
+    for column in ["rdisk_bb", "e_rdisk_bb"]:
         if len(sdb_helptab[column].mask.nonzero()[0]) > 0:
-            print('careful, masked entries in ', column)
-    disk_basic.rename_columns(['id', 'rdisk_bb', 'e_rdisk_bb', 'disks_ref'],
-                              ['main_id', 'rad_value', 'rad_err', 'rad_ref'])
-    disk_basic = disk_basic[np.where(np.isfinite(disk_basic['rad_value']))]
+            print("careful, masked entries in ", column)
+    disk_basic.rename_columns(
+        ["id", "rdisk_bb", "e_rdisk_bb", "disks_ref"],
+        ["main_id", "rad_value", "rad_err", "rad_ref"],
+    )
+    disk_basic = disk_basic[np.where(np.isfinite(disk_basic["rad_value"]))]
     return disk_basic
 
 
@@ -144,24 +154,29 @@ def provider_sdb(distance_cut_in_pc, data):
     :param float distance_cut_in_pc: Distance up to which stars are included.
     :param data: Table containing sdb data.
     :returns: Dictionary with names and astropy tables containing
-        reference data, provider data, object data, identifier data, object to 
+        reference data, provider data, object data, identifier data, object to
         object relation data and basic disk data.
     :rtype: dict(str,astropy.table.table.Table)
     """
-
     sdb = empty_dict.copy()
-    sdb['provider'] = create_provider_table('Grant Kennedy Disks',
-                                            'http://drgmk.com/sdb/',
-                                            'priv. comm.', '2024-02-09')
-    sdb_helptab = create_sdb_helpertable(distance_cut_in_pc,
-                                         sdb['provider']['provider_bibcode'][0], data)
+    sdb["provider"] = create_provider_table(
+        "Grant Kennedy Disks",
+        "http://drgmk.com/sdb/",
+        "priv. comm.",
+        "2024-02-09",
+    )
+    sdb_helptab = create_sdb_helpertable(
+        distance_cut_in_pc, sdb["provider"]["provider_bibcode"][0], data
+    )
 
-    sdb['h_link'] = create_h_link_table(sdb_helptab)
-    sdb_helptab['ids'] = sdb_helptab['id']  #because only single id per source given
-    sdb['objects'] = create_objects_table(sdb_helptab)
-    sdb['ident'] = create_ident_table(sdb_helptab)
-    sdb['sources'] = create_sdb_sources_table(sdb_helptab, sdb)
-    sdb['disk_basic'] = create_disk_basic_table(sdb_helptab)
+    sdb["h_link"] = create_h_link_table(sdb_helptab)
+    sdb_helptab["ids"] = sdb_helptab[
+        "id"
+    ]  # because only single id per source given
+    sdb["objects"] = create_objects_table(sdb_helptab)
+    sdb["ident"] = create_ident_table(sdb_helptab)
+    sdb["sources"] = create_sdb_sources_table(sdb_helptab, sdb)
+    sdb["disk_basic"] = create_disk_basic_table(sdb_helptab)
 
-    save(list(sdb.values()), ['sdb_' + element for element in list(sdb.keys())])
+    save(list(sdb.values()), ["sdb_" + element for element in list(sdb.keys())])
     return sdb
--- data_generation/life_td_data_generation/tests/test_provider_sdb.py
+++ data_generation/life_td_data_generation/tests/test_provider_sdb.py
@@ -1,41 +1,101 @@
-from provider.sdb import *
+from astropy.io.votable import parse_single_table
 from astropy.table import Table
-from astropy import io
+from provider.sdb import create_sdb_helpertable, provider_sdb
 from utils.io import Path
 
+# import pytest
+import numpy as np  # arrays
 
-#import pytest
-
-#@pytest.mark.dependency(depends=["test_fetch_main_id"])
+# @pytest.mark.dependency(depends=["test_fetch_main_id"])
 def test_create_sdb_helpertable():
-    distance_cut_in_pc = 5.
-    data = io.votable.parse_single_table(
-        Path().additional_data + "sdb_30pc_09_02_2024.xml").to_table()
-    sdb_disks = create_sdb_helpertable(distance_cut_in_pc, 'priv. comm.', data)
-    columns = ['main_id', 'id', 'sdbid', 'name', '_r', 'raj2000', 'dej2000', 'pmra', 'pmde', 'sdb_host_main_id',
-               'sp_type', 'sp_bibcode', 'plx_value', 'plx_err', 'plx_bibcode', 'type_short', 'type_long',
-               'star_comp_no', 'teff', 'e_teff', 'plx_arcsec', 'e_plx_arcsec', 'lstar', 'e_lstar', 'rstar', 'e_rstar',
-               'logg', 'e_logg', 'mh', 'e_mh', 'a_v', 'e_a_v', 'lstar_1pc', 'e_lstar_1pc', 'disk_r_comp_no', 'temp',
-               'e_temp', 'ldisk_lstar', 'e_ldisk_lstar', 'rdisk_bb', 'e_rdisk_bb', 'ldisk_1pc', 'e_ldisk_1pc', 'lam0',
-               'e_lam0', 'beta', 'e_beta', 'dmin', 'e_dmin', 'q', 'e_q', 'model_comps', 'parameters', 'evidence',
-               'chisq', 'dof', 'model_mtime', 'url', 'phot_url', 'mnest_url', 'model_url', 'type', 'disks_ref']
-    plx_in_mas_cut = 1000. / distance_cut_in_pc
+    distance_cut_in_pc = 5.0
+    data = parse_single_table(
+        Path().additional_data + "sdb_30pc_09_02_2024.xml"
+    ).to_table()
+    sdb_disks = create_sdb_helpertable(distance_cut_in_pc, "priv. comm.", data)
+    columns = [
+        "main_id",
+        "id",
+        "sdbid",
+        "name",
+        "_r",
+        "raj2000",
+        "dej2000",
+        "pmra",
+        "pmde",
+        "sdb_host_main_id",
+        "sp_type",
+        "sp_bibcode",
+        "plx_value",
+        "plx_err",
+        "plx_bibcode",
+        "type_short",
+        "type_long",
+        "star_comp_no",
+        "teff",
+        "e_teff",
+        "plx_arcsec",
+        "e_plx_arcsec",
+        "lstar",
+        "e_lstar",
+        "rstar",
+        "e_rstar",
+        "logg",
+        "e_logg",
+        "mh",
+        "e_mh",
+        "a_v",
+        "e_a_v",
+        "lstar_1pc",
+        "e_lstar_1pc",
+        "disk_r_comp_no",
+        "temp",
+        "e_temp",
+        "ldisk_lstar",
+        "e_ldisk_lstar",
+        "rdisk_bb",
+        "e_rdisk_bb",
+        "ldisk_1pc",
+        "e_ldisk_1pc",
+        "lam0",
+        "e_lam0",
+        "beta",
+        "e_beta",
+        "dmin",
+        "e_dmin",
+        "q",
+        "e_q",
+        "model_comps",
+        "parameters",
+        "evidence",
+        "chisq",
+        "dof",
+        "model_mtime",
+        "url",
+        "phot_url",
+        "mnest_url",
+        "model_url",
+        "type",
+        "disks_ref",
+    ]
+    plx_in_mas_cut = 1000.0 / distance_cut_in_pc
 
     assert sdb_disks.colnames == columns
     assert len(sdb_disks) > 0
-    assert sdb_disks['type'][0] == 'di'
-    assert sdb_disks['disks_ref'][0] == 'priv. comm.'
-    assert max(sdb_disks['plx_value']) > plx_in_mas_cut
+    assert sdb_disks["type"][0] == "di"
+    assert sdb_disks["disks_ref"][0] == "priv. comm."
+    assert max(sdb_disks["plx_value"]) > plx_in_mas_cut
 
 
 def test_provider_sdb():
-    distance_cut_in_pc = 5.
-    data = io.votable.parse_single_table(
-        Path().additional_data + "sdb_30pc_09_02_2024.xml").to_table()
+    distance_cut_in_pc = 5.0
+    data = parse_single_table(
+        Path().additional_data + "sdb_30pc_09_02_2024.xml"
+    ).to_table()
     sdb = provider_sdb(distance_cut_in_pc, data)
 
-    ref = np.array(['priv. comm.'])
-    provider_name = np.array(['Grant Kennedy Disks'])
-    example_table = Table((ref, provider_name), names=('ref', 'provider_name'))
+    ref = np.array(["priv. comm."])
+    provider_name = np.array(["Grant Kennedy Disks"])
+    example_table = Table((ref, provider_name), names=("ref", "provider_name"))
 
-    assert sdb['sources'] == example_table
+    assert sdb["sources"] == example_table
