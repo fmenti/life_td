3a1187367cf97ac8fe10d9988294321b5d34f140
Franziska Menti <fmenti@phys.ethz.ch>
2025-08-26 12:58:50 +0200

    cosmetic changes by tooling ruff to life_td and test files

--- data_generation/life_td_data_generation/life_td.py
+++ data_generation/life_td_data_generation/life_td.py
@@ -4,26 +4,30 @@ Generates the data for the LIFE Target Database.
 """
 
 from astropy import io
-
-#self created modules
-from sdata import empty_dict, empty_provider_tables_dict
-from utils.io import load, Path, string_to_object_whole_dict
+from building import building
 from provider.exo import provider_exo
 from provider.gaia import provider_gaia
 from provider.life import provider_life
 from provider.sdb import provider_sdb
 from provider.simbad import provider_simbad
 from provider.wds import provider_wds
-from building import building
+
+# self created modules
+from sdata import empty_dict, empty_provider_tables_dict
+from utils.io import Path, load, string_to_object_whole_dict
 
 
-#tbd change provider to provider_info to minimize confusion in building function with providers list
-def load_cat(provider_name=''):
+# tbd change provider to provider_info to minimize confusion in building function with providers list
+def load_cat(provider_name=""):
     cat = empty_dict.copy()
-    if provider_name == '':
-        prov = load([direction for direction in list(cat.keys())], location=Path().data)
+    if provider_name == "":
+        prov = load(
+            [direction for direction in list(cat.keys())], location=Path().data
+        )
     else:
-        prov = load([provider_name + '_' + direction for direction in list(cat.keys())])
+        prov = load(
+            [provider_name + "_" + direction for direction in list(cat.keys())]
+        )
     for i, table in enumerate(list(cat.keys())):
         cat[table] = prov[i]
     return cat
@@ -32,21 +36,20 @@ def load_cat(provider_name=''):
 def load_life_td():
     """
     Loads previously created life_td data.
-    
+
     :return: life_td data in different tables
     :rtype: list(astropy.table.table.Table)
     """
-
-    print(f'Loading life_td generated data')
+    print("Loading life_td generated data")
 
     provider_tables_dict = empty_provider_tables_dict.copy()
 
     for i, prov in enumerate(list(provider_tables_dict.keys())):
-        print(f'Loading {prov} data')
+        print(f"Loading {prov} data")
         cat = load_cat(prov)
         provider_tables_dict[prov] = string_to_object_whole_dict(cat)
 
-    db = load_cat('')
+    db = load_cat("")
     database_tables = string_to_object_whole_dict(db)
 
     return provider_tables_dict, database_tables
@@ -55,38 +58,52 @@ def load_life_td():
 def partial_create(distance_cut_in_pc, create=[]):
     """
     Partially generates, partially loads life_td data.
-    
-    Generates the in the create list specified life_td data, loads the 
+
+    Generates the in the create list specified life_td data, loads the
     rest and builds everything together.
-    
+
     :param distance_cut_in_pc: Distance cut of the objects in parsec.
     :type distance_cut_in_pc: float
     :param create: If one or more of 'sim', 'sdb', 'wds', 'exo', 'life',
-        and 'gaia'  are present, generates those tables, the missing 
+        and 'gaia'  are present, generates those tables, the missing
         ones are loaded.
     :type create: list(str)
     :return: life_td data in different tables
     :rtype: list(astropy.table.table.Table)
     """
-
-    # to do: solve error: 
+    # to do: solve error:
     # ValueError: Unit 'MJup' not supported by the CDS standard. Did you mean Mjup, YMjup, ZMjup, yMjup or zMjup? same for 'ercent'
-    with open(Path().data + 'distance_cut.txt', 'w') as f:
+    with open(Path().data + "distance_cut.txt", "w") as f:
         # Write the floating point number to the file
         f.write(str(distance_cut_in_pc))
 
     distance_cut_in_pc = float(distance_cut_in_pc)
 
-    print(f'Building life_td data with distance cut of {distance_cut_in_pc} pc')
+    print(f"Building life_td data with distance cut of {distance_cut_in_pc} pc")
 
     provider_tables_dict = empty_provider_tables_dict.copy()
     data = io.votable.parse_single_table(
-        Path().additional_data + "sdb_30pc_09_02_2024.xml").to_table()
-    functions = [provider_simbad, provider_sdb, provider_wds, provider_exo, provider_life, provider_gaia]
-    arguments = [(distance_cut_in_pc), (distance_cut_in_pc, data), (), (), (), (distance_cut_in_pc)]
+        Path().additional_data + "sdb_30pc_09_02_2024.xml"
+    ).to_table()
+    functions = [
+        provider_simbad,
+        provider_sdb,
+        provider_wds,
+        provider_exo,
+        provider_life,
+        provider_gaia,
+    ]
+    arguments = [
+        (distance_cut_in_pc),
+        (distance_cut_in_pc, data),
+        (),
+        (),
+        (),
+        (distance_cut_in_pc),
+    ]
 
     for i, prov in enumerate(list(provider_tables_dict.keys())):
-        #provider_tables_dict[prov]=string_to_object_whole_dict(provider_tables_dict[prov])
+        # provider_tables_dict[prov]=string_to_object_whole_dict(provider_tables_dict[prov])
         if prov in create:
             if arguments[i] != ():
                 try:
@@ -96,11 +113,11 @@ def partial_create(distance_cut_in_pc, create=[]):
             else:
                 cat = functions[i]()
         else:
-            print(f'Loading {prov} data')
+            print(f"Loading {prov} data")
             cat = load_cat(prov)
         provider_tables_dict[prov] = string_to_object_whole_dict(cat)
 
-    #------------------------combine data from external sources---------
+    # ------------------------combine data from external sources---------
     database_tables = building(provider_tables_dict)
     return provider_tables_dict, database_tables
 
@@ -108,21 +125,21 @@ def partial_create(distance_cut_in_pc, create=[]):
 def create_life_td(distance_cut_in_pc):
     """
     Generates the life_td data.
-    
+
     Calls the modules provider, helperfunctions and building.
-    
+
     :param distance_cut_in_pc: Distance cut of the objects in parsec.
     :type distance_cut_in_pc: float
     :return: life_td data in different tables
     :rtype: list(astropy.table.table.Table)
     """
-
-    #------------------------obtain data from external sources---------------------
-    provider_tables_dict, database_tables = partial_create(distance_cut_in_pc,
-                                                           create=['sim', 'sdb', 'wds', 'exo', 'life', 'gaia'])
+    # ------------------------obtain data from external sources---------------------
+    provider_tables_dict, database_tables = partial_create(
+        distance_cut_in_pc, create=["sim", "sdb", "wds", "exo", "life", "gaia"]
+    )
     return provider_tables_dict, database_tables
 
 
-if (__name__ == '__main__'):
-    print('Executing as standalone script')
-    create_life_td(5.)
+if __name__ == "__main__":
+    print("Executing as standalone script")
+    create_life_td(5.0)
--- data_generation/life_td_data_generation/tests/test_life_td.py
+++ data_generation/life_td_data_generation/tests/test_life_td.py
@@ -1,31 +1,38 @@
-from life_td import *
-from utils.io import save
-from astropy.table import Table, setdiff
-from sdata import empty_dict_wit_columns
 import numpy as np
+from astropy.table import Table, setdiff
 
+# self created modules
+from sdata import empty_dict_wit_columns
+from utils.io import string_to_object_whole_dict, save
+from life_td import load_cat
 
 def test_load_cat():
-    #data
-    #data
-    sptype = np.array(['dM3.51', 'dM3:', 'dM5.0'])
-    main_id = np.array(['G 227-48B', '* mu. Dra C', 'FBS 1415+456'])
-    example_table = Table((main_id, sptype), names=('main_id', 'sptype_string'))
+    # data
+    sptype = np.array(["dM3.51", "dM3:", "dM5.0"])
+    main_id = np.array(["G 227-48B", "* mu. Dra C", "FBS 1415+456"])
+    example_table = Table((main_id, sptype), names=("main_id", "sptype_string"))
     dictionary = empty_dict_wit_columns.copy()
-    dictionary['star_basic'] = example_table
+    dictionary["star_basic"] = example_table
     dictionary = string_to_object_whole_dict(dictionary, 400)
 
-    provider_name = 'test_cat'
-    save(list(dictionary.values()), [provider_name + '_' + element for element in list(dictionary.keys())])
+    provider_name = "test_cat"
+    save(
+        list(dictionary.values()),
+        [provider_name + "_" + element for element in list(dictionary.keys())],
+    )
 
-    #function
+    # function
     loaded_dict = load_cat(provider_name)
 
-    #assert
+    # assert
     for tablename in dictionary.keys():
-        if len(dictionary[tablename]) > 0:  #change to more once this works
+        if len(dictionary[tablename]) > 0:  # change to more once this works
             for columnname in dictionary[tablename].colnames:
-                assert len(setdiff(dictionary[tablename], loaded_dict[tablename])) == 0
+                assert (
+                    len(setdiff(dictionary[tablename], loaded_dict[tablename]))
+                    == 0
+                )
+
 
 # def test_partial_create():
 #     partial_create(distance_cut_in_pc,create=[])
