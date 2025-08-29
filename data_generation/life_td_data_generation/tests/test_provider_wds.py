0d5f6959c8d554b63d4cd0cae3ffd666e6aa33d9
Franziska Menti <fmenti@phys.ethz.ch>
2025-08-29 13:44:57 +0200

    minor change from tooling ruff for readability for wds provider and test file of it

--- data_generation/life_td_data_generation/tests/test_provider_wds.py
+++ data_generation/life_td_data_generation/tests/test_provider_wds.py
@@ -1,27 +1,30 @@
-from provider.wds import *
+from provider.wds import assign_names
 from astropy.table import Table
-from provider.utils import query
-
-
-def test_wds_queryable():
-    wds_provider = Table()
-    wds_provider['provider_url'] = ["http://tapvizier.u-strasbg.fr/TAPVizieR/tap"]
-    adql_query = ["""SELECT top 10 *
-                FROM "B/wds/wds" """]
-    wds = query(wds_provider['provider_url'][0], adql_query[0])
-    assert type(wds) == type(Table())
+from provider.wds import query
 
 
 def test_wds_columns_queryable():
     wds_provider = Table()
-    wds_provider['provider_url'] = ["http://tapvizier.u-strasbg.fr/TAPVizieR/tap"]
-    adql_query = ["""SELECT TOP 10
+    wds_provider["provider_url"] = [
+        "http://tapvizier.u-strasbg.fr/TAPVizieR/tap"
+    ]
+    adql_query = [
+        """SELECT TOP 10
                     WDS as wds_name, Comp as wds_comp,
-                    sep1 as wds_sep1, sep2 as wds_sep2, 
+                    sep1 as wds_sep1, sep2 as wds_sep2,
                     Obs1 as wds_obs1, Obs2 as wds_obs2
-                    FROM "B/wds/wds" """]
-    wds = query(wds_provider['provider_url'][0], adql_query[0])
-    assert wds.colnames == ['wds_name', 'wds_comp', 'wds_sep1', 'wds_sep2', 'wds_obs1', 'wds_obs2']
+                    FROM "B/wds/wds" """
+    ]
+    wds = query(wds_provider["provider_url"][0], adql_query[0])
+    assert wds.colnames == [
+        "wds_name",
+        "wds_comp",
+        "wds_sep1",
+        "wds_sep2",
+        "wds_obs1",
+        "wds_obs2",
+    ]
+
 
 def test_assign_names_trivial_binaries():
     """
@@ -30,23 +33,23 @@ def test_assign_names_trivial_binaries():
     # Input table with trivial binaries
     wds_helptab = Table(
         data=[
-            ['12345+6789'],  # wds_name
-            [''],  # wds_comp
+            ["12345+6789"],  # wds_name
+            [""],  # wds_comp
             [None],  # system_name
             [None],  # primary
-            [None]  # secondary
+            [None],  # secondary
         ],
-        names=['wds_name', 'wds_comp', 'system_name', 'primary', 'secondary'],
-        dtype=[object, object, object, object, object]
+        names=["wds_name", "wds_comp", "system_name", "primary", "secondary"],
+        dtype=[object, object, object, object, object],
     )
 
     # Apply function
     updated_table = assign_names(wds_helptab)
 
     # Check output
-    assert updated_table['system_name'][0] == 'WDS J12345+6789AB'
-    assert updated_table['primary'][0] == 'WDS J12345+6789A'
-    assert updated_table['secondary'][0] == 'WDS J12345+6789B'
+    assert updated_table["system_name"][0] == "WDS J12345+6789AB"
+    assert updated_table["primary"][0] == "WDS J12345+6789A"
+    assert updated_table["secondary"][0] == "WDS J12345+6789B"
 
 
 def test_assign_names_higher_order_multiples_two_components():
@@ -56,23 +59,23 @@ def test_assign_names_higher_order_multiples_two_components():
     # Input table with two components in 'wds_comp'
     wds_helptab = Table(
         data=[
-            ['12345+6789'],  # wds_name
-            ['CD'],  # wds_comp
+            ["12345+6789"],  # wds_name
+            ["CD"],  # wds_comp
             [None],  # system_name
             [None],  # primary
-            [None]  # secondary
+            [None],  # secondary
         ],
-        names=['wds_name', 'wds_comp', 'system_name', 'primary', 'secondary'],
-        dtype=[object, object, object, object, object]
+        names=["wds_name", "wds_comp", "system_name", "primary", "secondary"],
+        dtype=[object, object, object, object, object],
     )
 
     # Apply function
     updated_table = assign_names(wds_helptab)
 
     # Check output
-    assert updated_table['system_name'][0] == 'WDS J12345+6789CD'
-    assert updated_table['primary'][0] == 'WDS J12345+6789C'
-    assert updated_table['secondary'][0] == 'WDS J12345+6789D'
+    assert updated_table["system_name"][0] == "WDS J12345+6789CD"
+    assert updated_table["primary"][0] == "WDS J12345+6789C"
+    assert updated_table["secondary"][0] == "WDS J12345+6789D"
 
 
 def test_assign_names_higher_order_multiples_multiple_components():
@@ -82,23 +85,23 @@ def test_assign_names_higher_order_multiples_multiple_components():
     # Input table with multiple components in 'wds_comp'
     wds_helptab = Table(
         data=[
-            ['12345+6789'],  # wds_name
-            ['C,D'],  # wds_comp
+            ["12345+6789"],  # wds_name
+            ["C,D"],  # wds_comp
             [None],  # system_name
             [None],  # primary
-            [None]  # secondary
+            [None],  # secondary
         ],
-        names=['wds_name', 'wds_comp', 'system_name', 'primary', 'secondary'],
-        dtype=[object, object, object, object, object]
+        names=["wds_name", "wds_comp", "system_name", "primary", "secondary"],
+        dtype=[object, object, object, object, object],
     )
 
     # Apply function
     updated_table = assign_names(wds_helptab)
 
     # Check output
-    assert updated_table['system_name'][0] == 'WDS J12345+6789C,D'
-    assert updated_table['primary'][0] == 'WDS J12345+6789C'
-    assert updated_table['secondary'][0] == 'WDS J12345+6789D'
+    assert updated_table["system_name"][0] == "WDS J12345+6789C,D"
+    assert updated_table["primary"][0] == "WDS J12345+6789C"
+    assert updated_table["secondary"][0] == "WDS J12345+6789D"
 
 
 def test_assign_names_mixed_rows():
@@ -108,14 +111,14 @@ def test_assign_names_mixed_rows():
     # Input table with mixed rows
     wds_helptab = Table(
         data=[
-            ['12345+6789', '98765-4321', '44444+1111'],  # wds_name
-            ['', 'AB', 'X,Y'],  # wds_comp
+            ["12345+6789", "98765-4321", "44444+1111"],  # wds_name
+            ["", "AB", "X,Y"],  # wds_comp
             [None, None, None],  # system_name
             [None, None, None],  # primary
-            [None, None, None]  # secondary
+            [None, None, None],  # secondary
         ],
-        names=['wds_name', 'wds_comp', 'system_name', 'primary', 'secondary'],
-        dtype=[object, object, object, object, object]
+        names=["wds_name", "wds_comp", "system_name", "primary", "secondary"],
+        dtype=[object, object, object, object, object],
     )
 
     # Apply function
@@ -123,16 +126,16 @@ def test_assign_names_mixed_rows():
 
     # Check outputs
     # Row 1
-    assert updated_table['system_name'][0] == 'WDS J12345+6789AB'
-    assert updated_table['primary'][0] == 'WDS J12345+6789A'
-    assert updated_table['secondary'][0] == 'WDS J12345+6789B'
+    assert updated_table["system_name"][0] == "WDS J12345+6789AB"
+    assert updated_table["primary"][0] == "WDS J12345+6789A"
+    assert updated_table["secondary"][0] == "WDS J12345+6789B"
 
     # Row 2
-    assert updated_table['system_name'][1] == 'WDS J98765-4321AB'
-    assert updated_table['primary'][1] == 'WDS J98765-4321A'
-    assert updated_table['secondary'][1] == 'WDS J98765-4321B'
+    assert updated_table["system_name"][1] == "WDS J98765-4321AB"
+    assert updated_table["primary"][1] == "WDS J98765-4321A"
+    assert updated_table["secondary"][1] == "WDS J98765-4321B"
 
     # Row 3
-    assert updated_table['system_name'][2] == 'WDS J44444+1111X,Y'
-    assert updated_table['primary'][2] == 'WDS J44444+1111X'
-    assert updated_table['secondary'][2] == 'WDS J44444+1111Y'
+    assert updated_table["system_name"][2] == "WDS J44444+1111X,Y"
+    assert updated_table["primary"][2] == "WDS J44444+1111X"
+    assert updated_table["secondary"][2] == "WDS J44444+1111Y"
