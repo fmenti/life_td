1596a0a0c924377e4f60200dd7728e8a0a950fce
Franziska Menti <fmenti@phys.ethz.ch>
2025-09-17 14:16:20 +0200

    minor changes suggested by ruff for some test files

--- data_generation/life_td_data_generation/tests/test_analysis.py
+++ data_generation/life_td_data_generation/tests/test_analysis.py
@@ -1,42 +1,43 @@
-from utils.analysis.analysis import *
 from astropy.table import Table
-
+from utils.analysis.analysis import different_data
 
 
 # TODO wirte a test for each cat data
 def test_different_data_standard():
-    cat = Table(data=[[39, 40, 2.5],
-                      [1e20, 90, 81]],
-                names=['coo_dec','coo_err_angle'], 
-                dtype=[float, float])
+    cat = Table(
+        data=[[39, 40, 2.5], [1e20, 90, 81]],
+        names=["coo_dec", "coo_err_angle"],
+        dtype=[float, float],
+    )
 
-    colname = 'coo_dec'
-    arr=cat[colname]
-    
-    arr=different_data(arr)
+    colname = "coo_dec"
+    arr = cat[colname]
+
+    arr = different_data(arr)
 
     assert len(arr) == 3
     assert arr[0] == 39
     assert max(arr) == 40
-    
-def test_different_data_fill_values():
-    cat = Table(data=[[39, 40, 2.5],
-                      [1e20, 90, 81]],
-                names=['coo_dec','coo_err_angle'], 
-                dtype=[float, float])
 
-    colname = 'coo_err_angle'
-    arr=cat[colname]
-    
-    arr=different_data(arr)
+
+def test_different_data_fill_values():
+    cat = Table(
+        data=[[39, 40, 2.5], [1e20, 90, 81]],
+        names=["coo_dec", "coo_err_angle"],
+        dtype=[float, float],
+    )
+
+    colname = "coo_err_angle"
+    arr = cat[colname]
+
+    arr = different_data(arr)
 
     assert len(arr) == 2
     assert arr[0] == 90
     assert max(arr) == 90
-    
+
+    # next: do same like this function again for the different data columns so that all diff_data cases get tested.
 
 
-    #next: do same like this function again for the different data columns so that all diff_data cases get tested.
-
-#def test_2():
+# def test_2():
 #    assert False
