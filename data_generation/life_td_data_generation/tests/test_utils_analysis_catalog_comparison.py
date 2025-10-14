6278fd1f547fc35000e850deb882276c67c6b349
Franziska Menti <fmenti@phys.ethz.ch>
2025-10-14 14:08:27 +0200

    added test function for test_object_dropout though both function and test still have bugs

---
+++ data_generation/life_td_data_generation/tests/test_utils_analysis_catalog_comparison.py
@@ -0,0 +1,16 @@
+from utils.analysis.catalog_comparison import test_object_dropout
+from astropy.table import Table
+
+def test_test_object_dropout():
+    test_objects = Table(
+        data=[['name 2', 'name 3'], [1e20, 90]],
+        names=["main_id", "coo_err_angle"],
+        dtype=[object, float],)
+    parent_sample = Table(
+        data=[['name 1', 'name 2', 'name 4'], [1e20, 90, 81]],
+        names=["main_id", "coo_err_angle"],
+        dtype=[object, float],)
+
+    dropout, test_without_dropout = test_object_dropout(test_objects['main_id'],parent_sample['main_id'])
+    assert dropout == ['name 3']
+    assert test_without_dropout == ['name 2']
